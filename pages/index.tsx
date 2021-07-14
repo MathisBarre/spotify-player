ebdd0297e7364f465474df805861d8f62851e422
Mathis Barré <mathis.barre@live.fr>
2021-07-14 11:50:29 +0200

    feat: track list ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,34 @@
 import styled from "@emotion/styled"
 import Image from "next/image"
-import playlistImage from "../public/images/playlist.png"
+import PlaylistHeader from "../components/PlaylistHeader"
 import heartImage from "../public/images/heart.svg"
 
 export default function Home() {
   return (
     <Container>
-      <PlaylistHeader>
-        <Image alt="" src={playlistImage} />
-        <PlaylistDetails>
-          <h1>
-            <PlaylistTag>PLAYLIST</PlaylistTag><br />
-            <PlaylistTitle>Rituel Days 2019</PlaylistTitle>
-          </h1>
-          <PlaylistDescription>La playlist non officielle du festival</PlaylistDescription>
-          <PlaylistInformations>Created by <PlaylistAuthor href="#/user/sylvain-cr">Sylvain Cr</PlaylistAuthor> • 50 songs, 5 hr 38 min</PlaylistInformations>
-          <PlaylistActions>
-            <PlaylistPlayButton>PLAY</PlaylistPlayButton>
-            <PlaylistLikeButton>
-              <PlaylistLikeIcon as={Image} src="/images/heart.svg" height="20px" width="20px" />
-            </PlaylistLikeButton>
-          </PlaylistActions>
-        </PlaylistDetails>
-      </PlaylistHeader>
+      <PlaylistHeader />
+      <Tracks>
+        <TracksTable>
+          <thead>
+            <tr>
+              <TableLabel></TableLabel>
+              <TableLabel>Title</TableLabel>
+              <TableLabel>Artist</TableLabel>
+              <TableLabel>Album</TableLabel>
+              <TableLabel>Release date</TableLabel>
+            </tr>
+          </thead>
+          <tbody>
+            <TrackRow>
+              <TableCell><Image src={heartImage} alt="like unfilled" /></TableCell>
+              <TableCell>Madeleine - Original Mix</TableCell>
+              <TableCell>Konstantin Siberique</TableCell>
+              <TableCell>Secret Weapon</TableCell>
+              <TrackReleaseDate>2019-05-08</TrackReleaseDate>
+            </TrackRow>
+          </tbody>
+        </TracksTable>
+      </Tracks>
     </Container>
   )
 }
@@ -33,75 +39,37 @@ const Container = styled.main`
   color: white;
 `
 
-const PlaylistHeader = styled.header`
-  width: 100%;
-  background: linear-gradient(to bottom, #373737, #111111);
+const Tracks = styled.div`
   padding: 1rem;
-  display: flex;
 `
 
-const PlaylistDetails = styled.div`
-  display: flex;
-  justify-content: flex-end;
-  flex-direction: column;
-  padding: 0rem 2rem;
+const TracksTable = styled.table`
+  width: 100%;
+  box-sizing: content-box;
+  table-layout: auto;
+  overflow-wrap: break-word;
+  border-collapse: collapse;
 `
 
-const PlaylistTag = styled.span`
+const TableLabel = styled.th`
+  color: #A2A2A2;
+  text-align: left;
+  text-transform: uppercase;
   letter-spacing: .1em;
-  font-weight: 600;
-  color: #A2A2A2;
+  font-weight: 400;
+  font-size: 0.875rem;
 `
 
-const PlaylistTitle = styled.span`
-  font-size: 4rem;
-  font-weight: 900;
-  line-height: 1;
-`
-
-const PlaylistDescription = styled.p`
-  color: #A2A2A2;
-  margin: .75rem 0;
-  font-weight: 600;
-`
-
-const PlaylistInformations = styled.p`
+const TrackReleaseDate = styled.td`
   color: #A2A2A2;
   font-weight: 600;
-
 `
 
-const PlaylistAuthor = styled.a`
-  color: white;
+const TrackRow = styled.tr`
+  padding: 1rem 0;
+  border-bottom: 1px #1F1F1F solid
 `
 
-const PlaylistActions = styled.div`
-  margin-top: 1.5rem;
-  display: flex;
-`
-
-const PlaylistPlayButton = styled.button`
-  background-color: #1aba53;
-  border: none;
-  color: white;
-  padding: .5rem 2.5rem;
-  border-radius: 100rem;
-  font-weight: 600;
-  letter-spacing: 0.1em;
-  cursor: pointer;
-`
-
-const PlaylistLikeButton = styled.button`
-  border: 2px solid #CBCBCB;
-  background: transparent;
-  border-radius: 100rem;
-  height: 2rem;
-  width: 2rem;
-  margin-left: .5rem;
-  cursor: pointer;
-`
-
-const PlaylistLikeIcon = styled.img`
-  height: 1.5rem;
-  width: 1.5rem;
+const TableCell = styled.td`
+  padding: .5rem 0;
 `
