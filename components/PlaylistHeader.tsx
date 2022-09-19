70eb0b9a68ab7c6f533badacb771b14a6b9ff09b
Mathis Barré <m.barre@beapp.fr>
2022-09-19 19:29:48 +0200

    feat: PlaylistHeader responsive

--- components/PlaylistHeader.tsx
+++ components/PlaylistHeader.tsx
@@ -20,22 +20,22 @@ export default function PlaylistHeader({
   return (
     <header className="w-full bg-gradient-to-b from-spotifyGray-200 to-spotifyGray-600 p-4 flex">
       {/** no alt given by the api. Since the image do not contain crucial information(s), we can let an empty value **/}
-      <Image
-        alt=""
-        src={playlistImageUrl}
-        layout="fixed"
-        height="192"
-        width="192"
-      />
-      <div className="flex justify-center flex-col px-8 h-">
+      <div className="relative w-24 h-24">
+        <Image alt="" src={playlistImageUrl} layout="fill" />
+      </div>
+      <div className="flex justify-center flex-col pl-4">
         <h1>
-          <span className="tracking-widest text-spotifyGray-100">PLAYLIST</span>
+          <span className="tracking-widest text-spotifyGray-100 text-sm">
+            PLAYLIST
+          </span>
           <br />
-          <span className="text-6xl font-black leading-none">{name}</span>
+          <span className="text-2xl md:text-6xl font-black leading-none">
+            {name}
+          </span>
         </h1>
-        <div className="mt-6 flex">
+        <div className="mt-2  flex">
           <button
-            className="bg-spotifyPrimary border-none py-2 px-10 rounded-full font-semibold tracking-widest cursor-pointer"
+            className="bg-spotifyPrimary border-none py-2 px-10 rounded-full font-semibold tracking-widest cursor-pointer text-sm"
             onClick={playOrPausePlaylist}
           >
             {isPlaying ? "PAUSE" : "PLAY"}
